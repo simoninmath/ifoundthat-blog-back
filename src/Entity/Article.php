e39e7270bc2a72197e83f9bb38426f5302690630
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-20 15:50:07 +0100

    [api]-implement-attributs-into-article-entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -8,14 +8,31 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
-#[ApiResource]
+#[ApiResource (
+    operations: [
+        new Get(normalizationContext: ['groups' => ['read:Article:item']]),
+    ]
+)
+
+
+]
 class Article
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups ([
+        'read:Article:item'
+    ])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
