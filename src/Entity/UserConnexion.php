e13bb89305f8eced319c9a95e0412e5e2eccf8c0
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-20 16:45:18 +0100

    [api]-implements-apiplatform-attributs-into-userconnexion

--- src/Entity/UserConnexion.php
+++ src/Entity/UserConnexion.php
@@ -5,21 +5,51 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserConnexionRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
 
 #[ORM\Entity(repositoryClass: UserConnexionRepository::class)]
-#[ApiResource]
+#[ApiResource (
+    operations: [
+        new GetCollection(normalizationContext: ['groups' => ['read:UserConnexion:collection']]),
+        new Post(),
+        new Get(normalizationContext: ['groups' => ['read:UserConnexion:item']]),
+        new Put(),
+        new Delete(),
+        new Patch(),
+    ]
+)
+]
+
 class UserConnexion
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups ([
+        'read:UserConnexion:item',
+        'read:UserConnexion:collection'
+     ])]
     private ?int $id = null;
 
     #[ORM\Column]
+    #[Groups ([
+        'read:UserConnexion:item',
+        'read:UserConnexion:collection'
+     ])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'userConnexions')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups ([
+        'read:UserConnexion:item',
+        'read:UserConnexion:collection'
+     ])]
     private ?User $user = null;
 
     public function __construct()
