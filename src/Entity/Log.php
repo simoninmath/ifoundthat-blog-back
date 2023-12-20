7949c1eb7aa452b7561107cb0ca9ea57b15615ea
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-20 16:31:07 +0100

    [api]-implements-apiplatform-attributs-into-log

--- src/Entity/Log.php
+++ src/Entity/Log.php
@@ -5,20 +5,49 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\LogRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
 
 #[ORM\Entity(repositoryClass: LogRepository::class)]
-#[ApiResource]
+#[ApiResource (
+    operations: [
+        new GetCollection(normalizationContext: ['groups' => ['read:Log:collection']]),
+        new Post(),
+        new Get(normalizationContext: ['groups' => ['read:Log:item']]),
+        new Put(),
+        new Delete(),
+        new Patch(),
+    ]
+)
+]
 class Log
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups ([
+        'read:Log:item',
+        'read:Log:collection'
+     ])]
     private ?int $id = null;
 
     #[ORM\Column]
+    #[Groups ([
+        'read:Log:item',
+        'read:Log:collection'
+     ])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column]
+    #[Groups ([
+        'read:Log:item',
+        'read:Log:collection'
+     ])]
     private string $ipAddress = '';
 
     public function __construct()
