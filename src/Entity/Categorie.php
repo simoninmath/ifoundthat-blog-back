21d927e6570507eb1a0e3a5ed6bc45df3eae231d
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-20 16:22:40 +0100

    [api]-implements-apiplatform-attributs-into-categorie

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -7,20 +7,50 @@ use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
-#[ApiResource]
+#[ApiResource (
+    operations: [
+        new GetCollection(normalizationContext: ['groups' => ['read:Categorie:collection']]),
+        new Post(),
+        new Get(normalizationContext: ['groups' => ['read:Categorie:item']]),
+        new Put(),
+        new Delete(),
+        new Patch(),
+    ]
+)
+]
+
 class Categorie
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups ([
+        'read:Categorie:item',
+        'read:Categorie:collection'
+     ])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups ([
+        'read:Categorie:item',
+        'read:Categorie:collection'
+     ])]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'categorie', targetEntity: Article::class)]
+    #[Groups ([
+        'read:Categorie:item',
+        'read:Categorie:collection'
+     ])]
     private Collection $articles;
 
     public function __construct()
