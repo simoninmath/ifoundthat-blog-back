37ee4efaaddf5ac4fef0969c07c4e349f58883ba
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-21 11:02:12 +0100

    [api]-create-api-usercontroller

---
+++ src/Controller/Api/ApiUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+class ApiUserController extends AbstractController
+{
+    #[Route('/api/users', name: 'api_users', methods: ['GET'])]
+    public function getUsers(UserRepository $userRepository): JsonResponse
+    {
+        $users = $userRepository->findAll();  // Get all users
+
+        return $this->json($users);
+    }
+}
