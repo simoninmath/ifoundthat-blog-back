90bd5d3102663bba1ae66508237f05dad919c62e
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-21 10:13:09 +0100

    [api]-try-another-apicontroller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,20 +1,58 @@
 <?php
 
+// namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Component\Routing\Annotation\Route;
+// use App\Entity\User;
+// use Symfony\Component\HttpFoundation\Request;
+
+// class ApiController extends AbstractController
+// {
+
+//     #[Route('api/users', name:'api_users_list', methods: ['GET'])]
+//     public function getUsers(Request $request): JsonResponse
+//     {
+//         $userRepository = $request->get(User::class);
+//         $users = $userRepository->findAll();
+
+//         $formattedUsers = [];
+//         foreach ($users as $user) {
+//             $formattedUsers[] = [
+//                 'email' => $user->getEmail(),
+//                 'role' => $user->getRole(),
+//                 'createdAt' => $user->getCreatedAt()->format('Y-m-d H:i:s')
+//             ];
+//         }
+
+//         return $this->json($formattedUsers);
+//     }
+// }
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ApiController extends AbstractController
 {
+    private $doctrine;
 
-    #[Route('api/users', name:'api_users_list', methods: ['GET'])]
-    public function getUsers(Request $request): JsonResponse
+    public function __construct(ManagerRegistry $doctrine){
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/api/users', name: 'api_users_list', methods: ['GET'])]
+    public function getUsers(): JsonResponse
     {
-        $userRepository = $request->get(User::class);
+        $entityManager = $this->doctrine;
+        
+        $userRepository = $entityManager->getRepository(User::class);
+        
         $users = $userRepository->findAll();
 
         $formattedUsers = [];
