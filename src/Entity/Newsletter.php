2f6dc28da466a58bc6824674c6f473a97b80c940
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-26 10:55:14 +0100

    [setup]-commit-before-doctrine-upgrade

--- src/Entity/Newsletter.php
+++ src/Entity/Newsletter.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\NewsletterRepository;
 use Doctrine\ORM\Mapping as ORM;
+
 use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
