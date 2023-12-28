4a8bef4e7d9d049eea93efb7a335f89fbe1304f9
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-28 17:44:45 +0100

    [auth]-update-security-controller

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -3,14 +3,24 @@
 namespace App\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route(name: 'app_security_')]
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login()
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        // return $this->render();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUserName = $authenticationUtils->getLastUsername();
+        return $this->render('MON TEMPLATE ANGULAR', [
+            'error' => $error,
+            'lastUserName' => $lastUserName
+        ]);
     }
+
+    #[Route('/logout', name:'logout')]
+    public function logout() {}
 }
