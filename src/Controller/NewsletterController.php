f1e7e272e1a83cfd24aaf2e1d8fb0d12351770a3
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-15 14:22:26 +0100

    [contr]-update-newsletter-controller-with-routes

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,23 +2,22 @@
 
 namespace App\Controller;
 
-use App\Service\CustomApiService;
+use App\Service\NewsletterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsletterController extends AbstractController
 {
 
-    //TODO DI à corriger
+    //TODO mettre CRUD dans NewsletterService
     public function __construct(
-        private CustomApiService $customApiService  // use DI with controller for more than 1 method
+        private NewsletterService $newsletterService   // use DI with controller for more than 1 method
     ){} 
 
-    //TODO routes à modifier
-    #[Route('/api/newsletters', name: 'newsletter_users_from_api')]
+    #[Route('/api/public_newsletters_post', name: 'newsletter_users_from_api')]
     public function getNewsletters()
     {
-        $responseObject = $this->customApiService->getNewslettersApi();
+        $responseObject = $this->newsletterService->getUsersFromNewsletterAPI();
 
         return $responseObject;
     }
