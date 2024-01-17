e6d77b8be781c130caa8d49b70c0a775148ff57e
SIMONIN Mathieu <simonin.math@gmail.com>
2024-01-17 17:09:19 +0100

    [filter]-update-searchfilter-class

--- src/ApiFilter/SearchFilter.php
+++ src/ApiFilter/SearchFilter.php
@@ -8,11 +8,10 @@ use ApiPlatform\Metadata\Operation;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\PropertyInfo\Type;
 
-final class RegexpFilter extends AbstractFilter
+final class SearchFilter extends AbstractFilter
 {
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
-        // Otherwise filter is applied to order and page as well
         if (
             !$this->isPropertyEnabled($property, $resourceClass) ||
             !$this->isPropertyMapped($property, $resourceClass)
@@ -20,13 +19,12 @@ final class RegexpFilter extends AbstractFilter
             return;
         }
 
-        $parameterName = $queryNameGenerator->generateParameterName($property); // Generate a unique parameter name to avoid collisions with other filters
+        $parameterName = $queryNameGenerator->generateParameterName($property);
         $queryBuilder
             ->andWhere(sprintf('REGEXP(o.%s, :%s) = 1', $property, $parameterName))
             ->setParameter($parameterName, $value);
     }
 
-    // This function is only used to hook in documentation generators (supported by Swagger and Hydra)
     public function getDescription(string $resourceClass): array
     {
         if (!$this->properties) {
@@ -35,16 +33,16 @@ final class RegexpFilter extends AbstractFilter
 
         $description = [];
         foreach ($this->properties as $property => $strategy) {
-            $description["regexp_$property"] = [
+            $description["searchp_$property"] = [
                 'property' => $property,
                 'type' => Type::BUILTIN_TYPE_STRING,
                 'required' => false,
-                'description' => 'Filter using a regex. This will appear in the OpenApi documentation!',
+                'description' => 'Filter using a search. This will appear in the OpenApi documentation!',
                 'openapi' => [
                     'example' => 'Custom example that will be in the documentation and be the default value of the sandbox',
-                    'allowReserved' => false,// if true, query parameters will be not percent-encoded
+                    'allowReserved' => false,
                     'allowEmptyValue' => true,
-                    'explode' => false, // to be true, the type must be Type::BUILTIN_TYPE_ARRAY, ?product=blue,green will be ?product=blue&product=green
+                    'explode' => false,
                 ],
             ];
         }
@@ -52,44 +50,3 @@ final class RegexpFilter extends AbstractFilter
         return $description;
     }
 }
-
-
-
-// namespace App\ApiFilter;
-
-// use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
-// // use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractFilter;
-// use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
-// use Doctrine\ORM\QueryBuilder;
-
-// final class SearchFilter extends AbstractFilter
-// {
-//     protected function filterProperty(
-//         string $property,
-//         $value,
-//         QueryBuilder $queryBuilder,
-//         QueryNameGeneratorInterface $queryNameGenerator,
-//         string $resourceClass,
-//         string $operationName = null
-//     ) {
-//         // Appliquez votre logique de liste blanche ici
-//         // Assurez-vous de sécuriser correctement la recherche pour éviter les injections ou les abus
-//         if ('search' === $property && $this->isPropertyEnabled($property, $resourceClass)) {
-//             // Par exemple, ajoutez une condition WHERE pour filtrer la recherche
-//             $queryBuilder->andWhere('...'); // Votre condition ici
-//         }
-//     }
-
-//     public function getDescription(string $resourceClass): array
-//     {
-//         // Vous pouvez également fournir une description pour la documentation
-//         return [
-//             'search' => [
-//                 'property' => 'search',
-//                 'type' => 'string',
-//                 'required' => false,
-//                 'description' => 'Filter by search query.',
-//             ],
-//         ];
-//     }
-// }
